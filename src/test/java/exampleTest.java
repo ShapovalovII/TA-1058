7a310d2e96d6ac418b5c3f4303bf7a2640315044
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-10-25 12:07:11 +0300

    update test

--- src/test/java/exampleTest.java
+++ src/test/java/exampleTest.java
@@ -1,5 +1,6 @@
 import io.trueautomation.client.driver.TrueAutomationDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -11,24 +12,30 @@ import static io.trueautomation.client.TrueAutomationHelper.ta;
 
 public class exampleTest {
     private WebDriver driver;
-    private By loginBtn = By.xpath(ta("loginBtn", "//div[./span[text()='Login']]"));
-    private By signupBtn = By.cssSelector(ta("signupBtn", "div.sign-up-container > a"));
-    private By emailFl = By.name(ta("emailFl", "email"));
 
     @BeforeTest
     public void beforeTest() {
+        Dimension dimension = new Dimension(1640, 1000);
         driver = new TrueAutomationDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().window().setSize(dimension);
     }
 
     @Test
-    public void exampleTest() {
-        driver.get("https://trueautomation.io");
+    public void exampleTest() throws InterruptedException {
+        driver.get("https://calc.by/math-calculators/scientific-calculator.html");
 
-        driver.findElement(loginBtn).click();
-        driver.findElement(signupBtn).click();
+        driver.findElement(By.xpath(ta("Calc:1:c","//a[@id='btn_1']"))).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(ta("Calc:2:c", "//a[@id='btn_2']"))).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(ta("Calc:3:c", "//a[@id='btn_3']"))).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(ta("Calc:4:c", "//a[@id='btn_4']"))).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(ta("Calc:5:c", "//a[@id='btn_5']"))).click();
 
-        driver.findElement(emailFl).sendKeys("your@mail.com");
+        Thread.sleep(3000);
     }
 
     @AfterTest
